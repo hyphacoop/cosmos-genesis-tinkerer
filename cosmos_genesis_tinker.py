380f4c2c895ce9aacca20db4ccb702e94b923df6
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-06-21 12:16:57 -0400

    subprocess cmd

--- cosmos_genesis_tinker.py
+++ cosmos_genesis_tinker.py
@@ -336,9 +336,8 @@ class GenesisTinker:  # pylint: disable=R0902,R0904
         self.log_step("Creating preprocessing file " +
                       self.preprocessing_file)
         self._preprocessing = True
-        subprocess.run([
-                'jq', "'.'", self.input_file, '>', self.preprocessing_file],
-                check=True)
+        subprocess.run(f"jq '.' {self.input_file} > {self.preprocessing_file}",
+                check=True, shell=True)
         # shutil.copy2(self.input_file, self.preprocessing_file)
 
     def replace_delegator(self, old_delegator: Delegator, new_delegator: Delegator):
