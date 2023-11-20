0c268e0c6ea5289336628560b6b3b0d4828f19d4
Dante Sanchez <dante.a.sanchez@gmail.com>
2023-11-20 11:34:14 -0500

    fix long line

--- cosmos_genesis_tinker.py
+++ cosmos_genesis_tinker.py
@@ -391,7 +391,8 @@ class GenesisTinker:  # pylint: disable=R0902,R0904
 
         # Sort coins in bank balances
         self.log_step("Sorting balances coins")
-        subprocess.run(f"jq '.app_state.bank.balances |= map(.coins |= sort_by(.denom))' {self.preprocessing_file} > sorted.json",
+        subprocess.run(f"jq '.app_state.bank.balances |= map(.coins |= sort_by(.denom))' " + \
+                       f"{self.preprocessing_file} > sorted.json",
                 check=True, shell=True)
         subprocess.run(f"mv sorted.json {self.preprocessing_file}",
                 check=True, shell=True)
