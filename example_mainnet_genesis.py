21235df9ac8995f7d9b01bf8abad078e87d44407
ASoTNetworks <dev@aseriesoftubez.com>
2024-03-25 12:25:37 -0400

    add allow_ibc_client to mainnet genesis

--- example_mainnet_genesis.py
+++ example_mainnet_genesis.py
@@ -76,7 +76,10 @@ gentink.add_task(gentink.set_voting_period,
                  voting_period='60s')
 
 # Make redelegations faster
-gentink.add_task(gentink.set_unbonding_time,
-                 unbonding_time='1s')
+# gentink.add_task(gentink.set_unbonding_time,
+#                  unbonding_time='1s')
+
+gentink.add_task(gentink.add_allowed_ibc_client,
+                 allowed_ibc_client='09-localhost')
 
 gentink.run_tasks()
